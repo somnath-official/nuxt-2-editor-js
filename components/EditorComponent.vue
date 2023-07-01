69934f72795b05a93fd80138f62b0ce2abfd1497
somnathsardar <githubsomnath1996@gmail.com>
2023-07-01 19:42:51 +0530

    Added editor component

---
+++ components/EditorComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Editor JS</h1>
+</template>
+
+<script>
+export default {
+  name: 'EditorJs'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <editor-component />
 </template>
 
 <script>
+import EditorComponent from '../components/EditorComponent.vue'
 export default {
+  components: { EditorComponent },
   name: 'IndexPage'
 }
 </script>
